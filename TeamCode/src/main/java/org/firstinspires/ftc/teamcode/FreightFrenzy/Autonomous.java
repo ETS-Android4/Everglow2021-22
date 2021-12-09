1aa7ef6e4e6d07c339f6253a190800729d2ed26a
Barnacles101 <barrton2004@gmail.com>
2021-12-09 21:39:05 +0200

    duck system initiation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous.java
@@ -4,18 +4,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DrivingSystem;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DuckSystem;
 import org.firstinspires.ftc.teamcode.FreightFrenzy.Utils.EverglowGamepad;
 
 @TeleOp(name = "FirstAutonomous", group = "Linear Opmode")
 public class Autonomous extends LinearOpMode {
     DrivingSystem   drivingSystem;
     EverglowGamepad gamepad;
+    DuckSystem duckSystem;
+    int SecondClickY = 0;
 
     @Override
     public void runOpMode() {
         drivingSystem = new DrivingSystem(this);
         gamepad       = new EverglowGamepad(gamepad1);
-
+        duckSystem    = new DuckSystem(this);
 
         waitForStart();
 
@@ -30,7 +33,14 @@ public class Autonomous extends LinearOpMode {
             if (gamepad.buttonPress("b")) {
                 drivingSystem.rotateInPlace(90);
             }
-
+            if (gamepad.buttonPress("y")){
+                if(SecondClickY%2==0){
+                    duckSystem.runDuckWheel();
+                }
+                else {
+                    duckSystem.stopDuckWheel();
+                }
+            }
 
             gamepad.update();
             telemetry.update();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/DuckSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/DuckSystem.java
@@ -4,17 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class DuckSystem {
-    DcMotor dückMotor;
+    public final DcMotor dückMotor;
 
     public DuckSystem(LinearOpMode opMode) {
         this.dückMotor = opMode.hardwareMap.get(DcMotor.class, "duck");
     }
 
-    public void run() {
-        dückMotor.setPower(0.68);
+    public void runDuckWheel() {
+        dückMotor.setPower(0.3);
     }
 
-    public void stöp() {
+    public void stopDuckWheel() {
         dückMotor.setPower(0);
     }
 }
