c99dd4bf6b6f6dd3a929a31484bd592f7e403ca2
taltal0544 <73160570+taltal0544@users.noreply.github.com>
2021-12-09 21:59:58 +0200

    pull push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/ArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/ArmSystem.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.FreightFrenzy.Systems;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class ArmSystem {
+    DcMotor flyWheels;
+    DcMotor arm;
+
+    public ArmSystem(LinearOpMode opMode) {
+        this.flyWheels = opMode.hardwareMap.get(DcMotor.class, "flyWheels");
+        this.arm = opMode.hardwareMap.get(DcMotor.class, "arm");
+    }
+
+    public void pull() {
+        double position = flyWheels.getCurrentPosition();
+        flyWheels.setPower(0.5);
+        while (position < 268) {
+            position = flyWheels.getCurrentPosition();
+        }
+        flyWheels.setPower(0);
+    }
+
+    public void push() {
+        double position = flyWheels.getCurrentPosition();
+        flyWheels.setPower(-0.5);
+        while (position < 268) {
+            position = flyWheels.getCurrentPosition();
+        }
+        flyWheels.setPower(0);
+    }
 }
