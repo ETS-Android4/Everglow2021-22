ca7c85cf69986d3c73bb680ac26b4cc082f333dc
AmitRusso <russoamit@iasa-school.org.il>
2021-12-26 18:20:58 +0200

    merged opmode to have both driving and buttons.
    merged opmode to have both driving and buttons. also added spit and collect with triggers and spit can ovveride collect while it is running (not complete and not sure it works).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/OpMode1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/OpMode1.java
@@ -12,35 +12,39 @@ public class OpMode1 extends LinearOpMode {
     DrivingSystem   drivingSystem;
     EverglowGamepad gamepad;
     ArmSystem       armSystem;
-
+    boolean toggle;
     @Override
     public void runOpMode() {
         drivingSystem = new DrivingSystem(this);
         armSystem     = new ArmSystem(this);
         gamepad       = new EverglowGamepad(gamepad1);
-
+        toggle = false;
 
         waitForStart();
 
         while (opModeIsActive()) {
-//            drivingSystem.driveByJoystick(gamepad1.left_stick_x, gamepad1.left_stick_y,
-//                    gamepad1.right_stick_x);
+            drivingSystem.driveByJoystick(gamepad1.left_stick_x, gamepad1.left_stick_y,
+                    gamepad1.right_stick_x);
 
 //            armSystem.moveArm(-100 * (int) gamepad1.left_stick_y);
-            armSystem.arm.setPower(-100 * (int) gamepad1.left_stick_y);
+//            armSystem.arm.setPower(-100 * (int) gamepad1.left_stick_y);
 
             if (gamepad.buttonPress("a")) {
                 armSystem.moveArm(-100);
             }
             if (gamepad.buttonPress("b")) {
-                armSystem.moveArm(-800);
+                armSystem.moveArm(-1600);
             }
 
-            if(gamepad.buttonPress("x")){
+            if(gamepad.buttonPress("right_trigger")){
                 armSystem.collect();
+                toggle = true;
             }
-            if(gamepad.buttonPress("y")){
-                armSystem.spit();
+            if(gamepad.buttonPress("left_trigger")){
+                if(toggle){
+                    armSystem.spit();
+                    armSystem.spit();
+                }
             }
 
             gamepad.update();
