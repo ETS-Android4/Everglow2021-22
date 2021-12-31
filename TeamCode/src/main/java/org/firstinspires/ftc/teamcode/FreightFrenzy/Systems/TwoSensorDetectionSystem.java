056212a3a699a087d4c311fcf3f607a879824b96
Guy Knaan <guyknaan@gmail.com>
2021-12-31 17:24:37 +0200

    Add DetectionSystem which uses 2 sensors instead of 3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/TwoSensorDetectionSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/TwoSensorDetectionSystem.java
@@ -1,54 +1,48 @@
 package org.firstinspires.ftc.teamcode.FreightFrenzy.Systems;
 
+import static org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DetectionSystem.ERROR_THRESHOLD_CM;
+import static org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DetectionSystem.LEFT_TARGET_DISTANCE_CM;
+import static org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DetectionSystem.MID_TARGET_DISTANCE_CM;
+import static org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DetectionSystem.RIGHT_TARGET_DISTANCE_CM;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-public class DetectionSystem {
+/**
+ * A detection System that only uses a left and a right sensor.
+ * If both the left and right sensors returns values that are too large, then the scoring element is assumed to be in the middle.
+ */
+public class TwoSensorDetectionSystem {
+    LinearOpMode opMode;
+    DistanceSensor rightSensor;
+    DistanceSensor leftSensor;
 
-    public static final double LEFT_TARGET_DISTANCE_CM = 65;
-    public static final double MID_TARGET_DISTANCE_CM = 60;
-    public static final double RIGHT_TARGET_DISTANCE_CM = 65;
-    public static final double ERROR_THRESHOLD_CM = 30;
-
-    private final LinearOpMode opMode;
-    private final DistanceSensor leftSensor;
-    private final DistanceSensor midSensor;
-    private final DistanceSensor rightSensor;
-
-    public DetectionSystem(LinearOpMode opMode) {
+    public TwoSensorDetectionSystem(LinearOpMode opMode) {
         this.opMode = opMode;
         this.leftSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_left");
-        this.midSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_middle");
         this.rightSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_right");
     }
 
-    public ArmSystem.Floors findTargetFloor2(){
+    public ArmSystem.Floors findTargetFloor(){
         double distanceLeft = leftSensor.getDistance(DistanceUnit.CM);
-        double distanceMid = midSensor.getDistance(DistanceUnit.CM);
         double distanceRight = rightSensor.getDistance(DistanceUnit.CM);
-
         double errorLeft = Math.abs(LEFT_TARGET_DISTANCE_CM - distanceLeft);
-        double errorMid = Math.abs(MID_TARGET_DISTANCE_CM - distanceMid);
         double errorRight = Math.abs(RIGHT_TARGET_DISTANCE_CM - distanceRight);
 
         ArmSystem.Floors targetFloor;
-        if (errorLeft < errorMid && errorLeft < errorRight ){
-            targetFloor = ArmSystem.Floors.FIRST;
-        }else if (errorMid < errorLeft && errorMid < errorRight ){
+        if (errorLeft > ERROR_THRESHOLD_CM && errorRight > ERROR_THRESHOLD_CM){
+            targetFloor = ArmSystem.Floors.SECOND;
+        }else if (errorLeft < errorRight){
             targetFloor = ArmSystem.Floors.FIRST;
         }else {
-            targetFloor = ArmSystem.Floors.FIRST;
+            targetFloor = ArmSystem.Floors.THIRD;
         }
-
         opMode.telemetry.addData("distanceLeft", distanceLeft);
-        opMode.telemetry.addData("distanceMid", distanceMid);
         opMode.telemetry.addData("distanceRight", distanceRight);
         opMode.telemetry.addData("targetFloor", targetFloor);
         opMode.telemetry.update();
-
         return targetFloor;
-
     }
 }
