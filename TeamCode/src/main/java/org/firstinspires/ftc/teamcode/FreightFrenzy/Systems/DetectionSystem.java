b7de8e0bcd0befe69e14b690574e94c49f14a02a
Guy Knaan <guyknaan@gmail.com>
2021-12-31 17:02:15 +0200

    Fix DetectionSystem Accidentally using wrong sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/DetectionSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/DetectionSystem.java
@@ -7,24 +7,26 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class DetectionSystem {
 
-    private static final double LEFT_TARGET_DISTANCE_CM  = 65;
-    private static final double MID_TARGET_DISTANCE_CM   = 60;
+    private static final double LEFT_TARGET_DISTANCE_CM = 65;
+    private static final double MID_TARGET_DISTANCE_CM = 60;
     private static final double RIGHT_TARGET_DISTANCE_CM = 65;
 
-    private final LinearOpMode   opMode;
+    private final LinearOpMode opMode;
     private final DistanceSensor leftSensor;
     private final DistanceSensor midSensor;
+    private final DistanceSensor rightSensor;
 
     public DetectionSystem(LinearOpMode opMode) {
-        this.opMode      = opMode;
-        this.leftSensor  = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_left");
-        this.midSensor   = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_middle");
+        this.opMode = opMode;
+        this.leftSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_left");
+        this.midSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_middle");
+        this.rightSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_right");
     }
 
-    public ArmSystem.Floors findTargetFloor2() {
+    public ArmSystem.Floors findTargetFloor2(){
         double distanceLeft = leftSensor.getDistance(DistanceUnit.CM);
         double distanceMid = midSensor.getDistance(DistanceUnit.CM);
-        double distanceRight = leftSensor.getDistance(DistanceUnit.CM);
+        double distanceRight = rightSensor.getDistance(DistanceUnit.CM);
 
         double errorLeft = Math.abs(LEFT_TARGET_DISTANCE_CM - distanceLeft);
         double errorMid = Math.abs(MID_TARGET_DISTANCE_CM - distanceMid);
@@ -35,14 +37,12 @@ public class DetectionSystem {
         opMode.telemetry.addData("distanceRight", distanceRight);
         opMode.telemetry.update();
 
-        if (errorLeft < errorMid && errorLeft < errorRight) {
+        if (errorLeft < errorMid && errorLeft < errorRight ){
             return ArmSystem.Floors.FIRST;
-        } else if (errorMid < errorLeft && errorMid < errorRight) {
+        }else if (errorMid < errorLeft && errorMid < errorRight ){
             return ArmSystem.Floors.SECOND;
-        } else {
+        }else {
             return ArmSystem.Floors.THIRD;
         }
-
-
     }
 }
