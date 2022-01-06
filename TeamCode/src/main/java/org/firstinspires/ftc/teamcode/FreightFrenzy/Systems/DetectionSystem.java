d62212a9b665b6f3fc99bab8c36ad7e34dfa7dc6
Guy Knaan <guyknaan@gmail.com>
2022-01-07 00:20:00 +0200

    Make click on stick rotate slower, make robot move when detecting totem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/DetectionSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Systems/DetectionSystem.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.FreightFrenzy.Utils.TimeUtils;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Utils.MathUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetectionSystem {
 
@@ -12,17 +16,60 @@ public class DetectionSystem {
     public static final double RIGHT_TARGET_DISTANCE_CM = 65;
     public static final double ERROR_THRESHOLD_CM       = 30;
 
+    private static final double INFINITY = 9999999;
+
     private final LinearOpMode   opMode;
     public final DistanceSensor leftSensor;
     public final DistanceSensor rightSensor;
 
     private final ArmSystem arm;
 
+    private List<Double> errorLeftValues = new ArrayList<>();
+    private List<Double> errorRightValues = new ArrayList<>();
+
     public DetectionSystem(LinearOpMode opMode) {
         this.opMode      = opMode;
         this.leftSensor  = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_left");
         this.rightSensor = opMode.hardwareMap.get(DistanceSensor.class, "distance_sensor_right");
-        this.arm = new ArmSystem(opMode);
+        this.arm = new ArmSystem(opMode); // todo: this creates a new arm system, which can cause bugs since it's a seprate arm system and ArmSystem has local variables
+        reset();
+    }
+
+    public void reset(){
+        errorLeftValues = new ArrayList<>();
+        errorRightValues = new ArrayList<>();
+    }
+
+    public void scan(){
+        double distanceLeft = leftSensor.getDistance(DistanceUnit.CM);
+        double distanceRight = rightSensor.getDistance(DistanceUnit.CM);
+        double errorLeft = Math.abs(LEFT_TARGET_DISTANCE_CM - distanceLeft);
+        double errorRight = Math.abs(RIGHT_TARGET_DISTANCE_CM - distanceRight);
+        errorLeftValues.add(errorLeft);
+        errorRightValues.add(errorRight);
+    }
+
+    public ArmSystem.Floors findTargetFloorAfterScan(){
+        ArmSystem.Floors targetFloor;
+        Double minErrorRight = MathUtils.min(errorLeftValues);
+        Double minErrorLeft = MathUtils.min(errorRightValues);
+        if (minErrorLeft == null || minErrorRight == null){
+            // scan was never called before this function was called
+            opMode.telemetry.addLine("error: findTargetFloorAfterScan() called before scan was ever called.");
+            return ArmSystem.Floors.FIRST;
+        }
+        if (minErrorRight > ERROR_THRESHOLD_CM && minErrorLeft > ERROR_THRESHOLD_CM) {
+            targetFloor = ArmSystem.Floors.THIRD;
+        } else if (minErrorLeft < minErrorRight) {
+            targetFloor = ArmSystem.Floors.FIRST;
+        } else {
+            targetFloor = ArmSystem.Floors.SECOND;
+        }
+        opMode.telemetry.addData("errorLeft", minErrorLeft);
+        opMode.telemetry.addData("errorRight", minErrorRight);
+        opMode.telemetry.addData("targetFloor", targetFloor);
+        opMode.telemetry.update();
+        return targetFloor;
     }
 
     public ArmSystem.Floors findTargetFloor2() {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/TestDetectionSystemSimple.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.FreightFrenzy;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.ArmSystem;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DetectionSystem;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DrivingSystem;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Systems.DuckSystem;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Utils.EverglowGamepad;
+import org.firstinspires.ftc.teamcode.FreightFrenzy.Utils.TimeUtils;
+
+@Autonomous(name = "Simple Detection System Test", group = "Test")
+public class TestDetectionSystemSimple extends LinearOpMode {
+
+    // when the right stick is pressed on the controller, make the rotation slower by this factor.
+    DrivingSystem   drivingSystem;
+    ArmSystem       armSystem;
+    DuckSystem      duckSystem;
+    EverglowGamepad ourGamepad1;
+    EverglowGamepad ourGamepad2;
+    TouchSensor     touch;
+    DetectionSystem detectionSystem;
+    int             counter = 0;
+
+    @Override
+    public void runOpMode() {
+        drivingSystem = new DrivingSystem(this);
+        armSystem     = new ArmSystem(this);
+        duckSystem    = new DuckSystem(this);
+        detectionSystem = new DetectionSystem(this);
+        ourGamepad1   = new EverglowGamepad(gamepad1);
+        ourGamepad2   = new EverglowGamepad(gamepad2);
+        touch         = hardwareMap.get(TouchSensor.class, "touch");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            detectionSystem.reset();
+            armSystem.autonomousMoveArm(ArmSystem.Floors.FIRST);
+            TimeUtils.sleep(1000);
+            ArmSystem.Floors targetFloor = detectionSystem.findTargetFloor2();
+            telemetry.addData("targetFloor", targetFloor);
+            stop();
+        }
+    }
+}
